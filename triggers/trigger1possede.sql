258b0d9d6238b4c0001dc2e779c33830cf756008
Alexis Bassignot <misterbalise2@gmail.com>
2023-11-23 11:09:47 +0100

    finish possede

--- triggers/trigger1possede.sql
+++ triggers/trigger1possede.sql
@@ -6,6 +6,7 @@ BEGIN
     DECLARE
         @Code_produit varchar(5),
         @No_magasin int,
+        @Nb int,
         @action char(1)
 
     IF EXISTS (SELECT * FROM DELETED)
@@ -17,9 +18,24 @@ BEGIN
             SET @action = 'I'
         END
 
+
     SELECT @Code_produit = I.Code_produit,
            @No_magasin = I.No_magasin
     FROM inserted AS I
 
+    select @Nb = count(*) from POSSEDE P where @Code_produit= P.Code_produit
+    IF @Nb >= 3
+        BEGIN
+            RAISERROR (N'Le produit existe déja dans 3 magasins.',16,1)
+        end
+    ELSE IF @action='U'
+        BEGIN
+            RAISERROR (N'Un enregistrement de la table POSSEDE ne peux pas etre modifié.',16,1)
+        end
+    ELSE
+        BEGIN
+            INSERT Into POSSEDE VALUES (@No_magasin,@Code_produit)
+        end
+END
 
 
